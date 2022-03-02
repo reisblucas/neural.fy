60888806394c17de6df262d307ba6c0047ad56fe
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-03-02 01:48:41 -0300

    :lipstick: Not found easter egg page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import '../styles/notFound.css';
 
 class NotFound extends Component {
   render() {
+    const notFound = '404: Not found this page :(';
+
     return (
       <div>
         <div className="headerPattern patternPages" data-testid="page-not-found">
           <Header { ...this.props } />
-          <h1 className="titlePage">NotFound 404 page</h1>
+          <div className="patternPages notFoundCentered">
+            <h1 className="titlePage">{notFound}</h1>
+            <div className="easterEggDiv">
+              <img
+                src="https://preview.redd.it/8p9lu946wrb81.jpg?auto=webp&s=5038299dd1c159334be0c3bcc5e57af91a317b66"
+                alt=""
+                className="easterEggImage"
+              />
+            </div>
+          </div>
         </div>
       </div>
     );
