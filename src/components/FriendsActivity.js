563e275b20b09a17368b618b3abb424426cfe065
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-03-18 16:33:58 -0300

    :sparkles: Friend Activity Default - Done

--- src/components/FriendsActivity.js
+++ src/components/FriendsActivity.js
@@ -1,32 +1,17 @@
-import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { Component } from 'react';
 import '../styles/friendsActivity.css';
-
-// Images imports
-import capuchinMonkey1 from '../images/friendsActivityProfile/capuchin-monkey-1.webp';
-import capuchinMonkey2 from '../images/friendsActivityProfile/capuchin-monkey-2.webp';
-import cattoFisherhat from '../images/friendsActivityProfile/catto-inside-fisherhat.jpg';
-import chonkyGreyCat from '../images/friendsActivityProfile/chonky-grey-cat.png';
-import couplePhoto from '../images/friendsActivityProfile/couple-photo.jpeg';
-import cryptopunkBlue from '../images/friendsActivityProfile/cryptopunk-blue.jpg';
-import chonkyOrangeCat from '../images/friendsActivityProfile/fat-cat.webp';
-import fortSuperHero from '../images/friendsActivityProfile/fortnite-superhero.webp';
-import eiffelTower from '../images/friendsActivityProfile/french-eiffel-tower.jpg';
-import frenchVisa from '../images/friendsActivityProfile/french-visa-1920x1080.jpg';
-import girlPic from '../images/friendsActivityProfile/girl-picture.jpeg';
-import logitechLogo from '../images/friendsActivityProfile/logitech-logo.png';
-import nftBieber from '../images/friendsActivityProfile/nft-bieber.jpg';
-import personInACarTrip from '../images/friendsActivityProfile/person-in-trip.jpeg';
-import personInACarTrip2 from '../images/friendsActivityProfile/person-in-cartrip-2.jpg';
-import pugHappy from '../images/friendsActivityProfile/pug-happy-3.webp';
-import pugLyingDownHappy from '../images/friendsActivityProfile/pug-happy.jpg';
-import pugSeatedHappy from '../images/friendsActivityProfile/pug-profile-happy.jpg';
-import sunshine from '../images/friendsActivityProfile/sunshie.jpeg';
-import vacation from '../images/friendsActivityProfile/vacation.png';
+import Input from './Input';
 
 export default class FriendsActivity extends Component {
+  state = {
+    hasFriendActivity: false,
+  }
+
   render() {
+    const { hasFriendActivity } = this.state;
+
     return (
       <div className="friends-container-hero">
         <div className="headerActFrnd">
@@ -43,154 +28,79 @@ export default class FriendsActivity extends Component {
           - Nome da Playlist
 
         */}
-        <img src={ vacation } width="200 " alt="teste" />
+        {
+          !hasFriendActivity
+            ? (
+              <div className="noHover">
+                <div className="subtitle">
+                  <p>Let friends and followers on Spotify</p>
+                  <p>see what you&apos;re listening to.</p>
+                </div>
+
+                <div className="activities-container">
+                  <div className="friend-activity">
+                    <div className="friend-profile-picture">
+                      <FontAwesomeIcon icon={ faUser } />
+                    </div>
+                    <div>
+                      <p className="friend-name friend-no-activity-nm" />
+                    </div>
+
+                    <div className="info-friend-music">
+                      <p className="friend-music-name friend-no-activity-am" />
+                      <p className="friend-artist-name friend-no-activity-am" />
+                    </div>
+                  </div>
+
+                  <div className="friend-activity">
+                    <div className="friend-profile-picture">
+                      <FontAwesomeIcon icon={ faUser } />
+                    </div>
+                    <div>
+                      <p className="friend-name friend-no-activity-nm" />
+                      <p className="friend-music-name friend-no-activity-am" />
+                      <p className="friend-artist-name friend-no-activity-am" />
+                    </div>
+                  </div>
+
+                  <div className="friend-activity">
+                    <div className="friend-profile-picture">
+                      <FontAwesomeIcon
+                        icon={ faUser }
+                        className="fa-profile-no-activity"
+                      />
+                    </div>
+                    <div>
+                      <p className="friend-name friend-no-activity-nm" />
+                      <p className="friend-music-name friend-no-activity-am" />
+                      <p className="friend-artist-name friend-no-activity-am" />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="subtitle">
+                  <p>Go to Seetings &gt; Social and enable</p>
+                  <p>&apos;Share my listening activity on</p>
+                  <p>Spotify.&apos; You can turn this off at any</p>
+                  <p>time.</p>
+                </div>
+
+                <div className="divButtonSettings">
+                  <Input
+                    type="button"
+                    value="S E T T I N G S"
+                    className="buttonSettings"
+                  />
+                </div>
+              </div>
+
+            )
+            : (
+              <div>teste</div>
+            )
+        }
+
       </div>
     );
   }
 }
-
-const idAlbumData = [
-  { 1440642493: 'Take Care (Deluxe Version)' },
-  { 1451901307: 'Graduation' },
-  { 1578986656: 'Cold Heart (PNAU Remix) - Single' },
-  { 395711243: 'Mozart - 100 Supreme Classical Masterpieces: Rise of the Masters' },
-  { 1440841363: 'Views' },
-  { 1423284802: 'Cross Road' },
-  { 273714443: 'Toxicity' },
-  { 1440783617: 'Nevermind' },
-  { 1442845496: 'Construção' },
-  { 1421241217: 'ASTROWORLD' },
-  { 1462912754: 'My Girl - Single' },
-  { 1346689058: 'Wild Kidz - Single' },
-  { 1440763429: 'Greatest Hits' },
-  { 203880450: 'The Best of Depeche Mode, Vol. 1 (Deluxe Version)' },
-  { 824700396: 'The Devil\'s Walk' },
-  { 266222258: 'A Flock of Seagulls' },
-  { 933067562: 'Dancin (feat. Luvli) [Krono Remix] - Single' },
-  { 574043989: 'Highway to Hell' },
-  { 902620286: 'Led Zeppelin IV (Deluxe Edition)' },
-  { 1440650816: 'A Night at the Opera (Deluxe Edition)' },
-];
-
-const musicData = [
-  {
-    image: fortSuperHero,
-    username: 'Lucca Baker',
-    musicName: 'Over My Dead Body',
-    artistName: 'Drake',
-  },
-  {
-    image: personInACarTrip,
-    username: 'Viktoria Martin',
-    musicName: 'Stronger',
-    artistName: 'Kanye West',
-  },
-  {
-    image: nftBieber,
-    username: 'Stephan Ferguson',
-    musicName: 'Cold Heart',
-    artistName: 'Elton John & Dua Lipa',
-  },
-  {
-    image: cattoFisherhat,
-    username: 'John Antony',
-    musicName: 'Serenade No. 13 in G Major, K. 525, "Eine Kleine Nachtmusik": I. Allegro',
-    artistName: 'Drottningholm Baroque Ensemble',
-  },
-  {
-    image: capuchinMonkey1,
-    username: 'Julio Freeman',
-    musicName: 'Hotline Bling',
-    artistName: 'Drake',
-  },
-  {
-    image: capuchinMonkey2,
-    username: 'Lucian Freeman',
-    musicName: 'Livin\' On a Prayer',
-    artistName: 'Bon Jovi',
-  },
-  {
-    image: chonkyGreyCat,
-    username: 'Matthew Gordon',
-    musicName: 'Toxicity',
-    artistName: 'System of A Down',
-  },
-  {
-    image: couplePhoto,
-    username: 'Artur Powell',
-    musicName: 'Smells Like Teen Spirit',
-    artistName: 'Nirvana',
-  },
-  {
-    image: cryptopunkBlue,
-    username: 'Raphael Larson',
-    musicName: 'Cotidiano',
-    artistName: 'Chico Buarque',
-  },
-  {
-    image: frenchVisa,
-    username: 'Matthew Baker',
-    musicName: 'SICK MODE',
-    artistName: 'Travis Scott',
-  },
-  {
-    image: eiffelTower,
-    username: 'Anthony',
-    musicName: 'Wild Kidz',
-    artistName: 'Vintage Culture & Ricci',
-  },
-  {
-    image: chonkyOrangeCat,
-    username: 'Giorgia Campbell',
-    musicName: 'My Girl',
-    artistName: 'Vintage Culture & Fancy Inc',
-  },
-  {
-    image: logitechLogo,
-    username: 'Tales Scott',
-    musicName: 'Hit \'Em Up (feat. Outlawz)',
-    artistName: 'Tupac Shakur',
-  },
-  {
-    image: girlPic,
-    username: 'Beatrice Franklin',
-    musicName: 'Enjoy the Silence',
-    artistName: 'Depeche Mode',
-  },
-  {
-    image: personInACarTrip2,
-    username: 'Beatrice Rogers',
-    musicName: 'Goodbye (with Soap & Skin)',
-    artistName: 'Apparat',
-  },
-  {
-    image: pugHappy,
-    username: 'James Carian',
-    musicName: 'I Ran (So Far Away)',
-    artistName: 'A Flock of Seagulls',
-  },
-  {
-    image: pugLyingDownHappy,
-    username: 'Gaius Campbell',
-    musicName: 'Dancin (feat. Luvli)',
-    artistName: 'Aaron Smith',
-  },
-  {
-    image: pugSeatedHappy,
-    username: 'Peter Scott',
-    musicName: 'Highway to Hell',
-    artistName: 'AC/DC',
-  },
-  {
-    image: sunshine,
-    username: 'Carl Murphy',
-    musicName: 'Stairway to Heaven',
-    artistName: 'Led Zeppelin',
-  },
-  {
-    image: vacation,
-    username: 'Alan Patterson',
-    musicName: 'Bohemian Rhapsody',
-    artistName: 'Queen',
-  },
-];
---
+++ src/data/friendsActivity/friendsData.js
@@ -0,0 +1,167 @@
+// Images imports
+import capuchinMonkey1 from '../../images/friendsActivityProfile/capuchin-monkey-1.webp';
+import capuchinMonkey2 from '../../images/friendsActivityProfile/capuchin-monkey-2.webp';
+import cattoFisherhat from '../../images/friendsActivityProfile/catto-fisherhat.jpg';
+import chonkyGreyCat from '../../images/friendsActivityProfile/chonky-grey-cat.png';
+import couplePhoto from '../../images/friendsActivityProfile/couple-photo.jpeg';
+import cryptopunkBlue from '../../images/friendsActivityProfile/cryptopunk-blue.jpg';
+import chonkyOrangeCat from '../../images/friendsActivityProfile/fat-cat.webp';
+import fortSuperHero from '../../images/friendsActivityProfile/fortnite-superhero.webp';
+import eiffelTower from '../../images/friendsActivityProfile/french-eiffel-tower.jpg';
+import frenchVisa from '../../images/friendsActivityProfile/french-visa-1920x1080.jpg';
+import girlPic from '../../images/friendsActivityProfile/girl-picture.jpeg';
+import logitechLogo from '../../images/friendsActivityProfile/logitech-logo.png';
+import nftBieber from '../../images/friendsActivityProfile/nft-bieber.jpg';
+import personInACarTrip from '../../images/friendsActivityProfile/person-in-trip.jpeg';
+import personInACarTrip2 from '../../images/friendsActivityProfile/person-cartrip-2.jpg';
+import pugHappy from '../../images/friendsActivityProfile/pug-happy-3.webp';
+import pugLyingDownHappy from '../../images/friendsActivityProfile/pug-happy.jpg';
+import pugSeatedHappy from '../../images/friendsActivityProfile/pug-profile-happy.jpg';
+import sunshine from '../../images/friendsActivityProfile/sunshie.jpeg';
+import vacation from '../../images/friendsActivityProfile/vacation.png';
+
+export const idAlbumData = [
+  { 1440642493: 'Take Care (Deluxe Version)' },
+  { 1451901307: 'Graduation' },
+  { 1578986656: 'Cold Heart (PNAU Remix) - Single' },
+  { 395711243: 'Mozart - 100 Supreme Classical Masterpieces: Rise of the Masters' },
+  { 1440841363: 'Views' },
+  { 1423284802: 'Cross Road' },
+  { 273714443: 'Toxicity' },
+  { 1440783617: 'Nevermind' },
+  { 1442845496: 'Construção' },
+  { 1421241217: 'ASTROWORLD' },
+  { 1462912754: 'My Girl - Single' },
+  { 1346689058: 'Wild Kidz - Single' },
+  { 1440763429: 'Greatest Hits' },
+  { 203880450: 'The Best of Depeche Mode, Vol. 1 (Deluxe Version)' },
+  { 824700396: 'The Devil\'s Walk' },
+  { 266222258: 'A Flock of Seagulls' },
+  { 933067562: 'Dancin (feat. Luvli) [Krono Remix] - Single' },
+  { 574043989: 'Highway to Hell' },
+  { 902620286: 'Led Zeppelin IV (Deluxe Edition)' },
+  { 1440650816: 'A Night at the Opera (Deluxe Edition)' },
+];
+
+export const musicData = [
+  {
+    image: fortSuperHero,
+    username: 'Lucca Baker',
+    musicName: 'Over My Dead Body',
+    artistName: 'Drake',
+  },
+  {
+    image: personInACarTrip,
+    username: 'Viktoria Martin',
+    musicName: 'Stronger',
+    artistName: 'Kanye West',
+  },
+  {
+    image: nftBieber,
+    username: 'Stephan Ferguson',
+    musicName: 'Cold Heart',
+    artistName: 'Elton John & Dua Lipa',
+  },
+  {
+    image: cattoFisherhat,
+    username: 'John Antony',
+    musicName: 'Serenade No. 13 in G Major, K. 525, "Eine Kleine Nachtmusik": I. Allegro',
+    artistName: 'Drottningholm Baroque Ensemble',
+  },
+  {
+    image: capuchinMonkey1,
+    username: 'Julio Freeman',
+    musicName: 'Hotline Bling',
+    artistName: 'Drake',
+  },
+  {
+    image: capuchinMonkey2,
+    username: 'Lucian Freeman',
+    musicName: 'Livin\' On a Prayer',
+    artistName: 'Bon Jovi',
+  },
+  {
+    image: chonkyGreyCat,
+    username: 'Matthew Gordon',
+    musicName: 'Toxicity',
+    artistName: 'System of A Down',
+  },
+  {
+    image: couplePhoto,
+    username: 'Artur Powell',
+    musicName: 'Smells Like Teen Spirit',
+    artistName: 'Nirvana',
+  },
+  {
+    image: cryptopunkBlue,
+    username: 'Raphael Larson',
+    musicName: 'Cotidiano',
+    artistName: 'Chico Buarque',
+  },
+  {
+    image: frenchVisa,
+    username: 'Matthew Baker',
+    musicName: 'SICK MODE',
+    artistName: 'Travis Scott',
+  },
+  {
+    image: eiffelTower,
+    username: 'Anthony',
+    musicName: 'Wild Kidz',
+    artistName: 'Vintage Culture & Ricci',
+  },
+  {
+    image: chonkyOrangeCat,
+    username: 'Giorgia Campbell',
+    musicName: 'My Girl',
+    artistName: 'Vintage Culture & Fancy Inc',
+  },
+  {
+    image: logitechLogo,
+    username: 'Tales Scott',
+    musicName: 'Hit \'Em Up (feat. Outlawz)',
+    artistName: 'Tupac Shakur',
+  },
+  {
+    image: girlPic,
+    username: 'Beatrice Franklin',
+    musicName: 'Enjoy the Silence',
+    artistName: 'Depeche Mode',
+  },
+  {
+    image: personInACarTrip2,
+    username: 'Beatrice Rogers',
+    musicName: 'Goodbye (with Soap & Skin)',
+    artistName: 'Apparat',
+  },
+  {
+    image: pugHappy,
+    username: 'James Carian',
+    musicName: 'I Ran (So Far Away)',
+    artistName: 'A Flock of Seagulls',
+  },
+  {
+    image: pugLyingDownHappy,
+    username: 'Gaius Campbell',
+    musicName: 'Dancin (feat. Luvli)',
+    artistName: 'Aaron Smith',
+  },
+  {
+    image: pugSeatedHappy,
+    username: 'Peter Scott',
+    musicName: 'Highway to Hell',
+    artistName: 'AC/DC',
+  },
+  {
+    image: sunshine,
+    username: 'Carl Murphy',
+    musicName: 'Stairway to Heaven',
+    artistName: 'Led Zeppelin',
+  },
+  {
+    image: vacation,
+    username: 'Alan Patterson',
+    musicName: 'Bohemian Rhapsody',
+    artistName: 'Queen',
+  },
+];
