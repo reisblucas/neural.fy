64b3cba694076d42ab828fc54db35dfd0e9ecbcf
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-03-22 15:19:07 -0300

    :fire: Remove favHeader reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
-import favHeader from './favHeader';
 import responseMusics from './responseMusics';
 import searchAlbum from './searchAlbum';
 
 const rootReducer = combineReducers({
   searchAlbum,
   responseMusics,
-  favHeader,
 });
 
 export default rootReducer;
