d1db18c78e7c53ecad9c458453809dc473ed6c0d
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-04-07 23:00:36 -0300

    :lipstick: FriendsActivity: Friend and Artist nam, undeline

---
+++ src/components/PlayerBottomSide.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import '../styles/playerBottomSide.css';
+
+export const PlayerBottomSide = (props) => (
+  <div className="player-container">
+    <div className="music-player-info">
+      <p className="music-name-player tdh tc">Music name</p>
+      <p className="artist-name-player tdh tc">Artist</p>
+    </div>
+
+    <div className="central-player-buttons">
+      <div>player</div>
+      <p>lagalagalgalalga</p>
+    </div>
+
+    <div className="right-player-buttons">
+      <div>buttons / volume</div>
+    </div>
+  </div>
+);
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PlayerBottomSide);
