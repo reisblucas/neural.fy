b2135c5e3f525114ec0a92d3dcf805d64297d5b4
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-04-08 20:51:56 -0300

    :art: style: heartIcon in player

--- src/components/PlayerBottomSide.js
+++ src/components/PlayerBottomSide.js
@@ -1,3 +1,5 @@
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { connect } from 'react-redux';
 import '../styles/playerBottomSide.css';
@@ -5,8 +7,13 @@ import '../styles/playerBottomSide.css';
 export const PlayerBottomSide = () => (
   <div className="player-container">
     <div className="music-player-info">
-      <p className="music-name-player tdh tc">Music name</p>
-      <p className="artist-name-player tdh tc">Artist</p>
+      <div className="artist-infos">
+        <p className="music-name-player tdh tc">Music name</p>
+        <p className="artist-name-player tdh tc">Artist</p>
+      </div>
+      <div className="favorite-player-container">
+        <FontAwesomeIcon icon={ faHeart } className="heartIcon-player" />
+      </div>
     </div>
 
     <div className="central-player-buttons">
