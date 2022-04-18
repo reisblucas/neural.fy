d3e00638b8a0281d173d8f96f1e90cbdc59ab0e9
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-04-18 14:40:02 -0300

    :fire: fix: remove fetch function for youtube api

--- src/services/youtubeAPI.js
+++
@@ -1,9 +0,0 @@
-export const getVideo = async (query) => {
-  const url = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyCmqsyYb-3RDBlQQHqJa_NgZwcjkmeJptQ&type=video&videoEmbeddable=true&q=${query}`;
-
-  const request = await fetch(url);
-  const response = await request.json();
-  console.log(response);
-};
-
-export const test = 'lucas';
