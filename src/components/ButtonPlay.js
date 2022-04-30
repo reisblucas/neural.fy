9a4c156c05d45eb3b52b3288133a72a6d3a0cef5
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-04-29 22:18:32 -0300

    :sparkles: feat: pause button now is persistent in album/fav

--- src/components/ButtonPlay.js
+++ src/components/ButtonPlay.js
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types';
-import { faPause, faPlay } from '@fortawesome/free-solid-svg-icons';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { GiPauseButton } from 'react-icons/gi';
 import { playAudio } from '../helpers/play-pause-inside-album-or-fav/player';
 
 class ButtonPlay extends Component {
@@ -18,16 +19,24 @@ class ButtonPlay extends Component {
 
     return (
       <div className="divTrackNumber">
-        <p className="trackNumber font-link">{ i + 1 }</p>
+
+        {
+          name === previewUrl && songGlobStatus
+            ? (
+              <GiPauseButton
+                className="trackNumber trackPauseIcon fs-15"
+              />
+            )
+            : <p className="trackNumber font-link">{ i + 1 }</p>
+        }
 
         <label htmlFor={ previewUrl }>
           {
             (name === previewUrl) && (songGlobStatus)
               ? (
-                <FontAwesomeIcon
+                <GiPauseButton
                   name={ previewUrl }
-                  icon={ faPause }
-                  className="focusable trackPlayIcon trackPauseIcon"
+                  className="focusable trackPlayIcon trackPauseIcon fs-15"
                   onClick={ (e) => {
                     handlePauseIcon(e);
                     playAudio(e);
