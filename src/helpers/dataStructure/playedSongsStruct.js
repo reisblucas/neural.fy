7e0e1f7c7e113b7f3161af0150ea9434c3335a55
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-05-01 04:11:24 -0300

    :poop: smell-c: playedSongsStruct

--- src/helpers/dataStructure/playedSongsStruct.js
+++ src/helpers/dataStructure/playedSongsStruct.js
@@ -1,8 +1,8 @@
 const playedSongsStruct = (trackData, songs, songUrl, setPlayedSongs) => {
   if (trackData === 'Do nothing!') {
-    const trackFilter = songs.filter(({ previewUrl }) => previewUrl === songUrl)[0];
-    setPlayedSongs({ ...trackFilter, status: true, name: songUrl });
-    return { ...trackFilter, status: true, name: songUrl };
+    const trackFilter2 = songs.filter(({ previewUrl }) => previewUrl === songUrl)[0];
+    setPlayedSongs({ ...trackFilter2, status: true, name: songUrl });
+    return { ...trackFilter2, status: true, name: songUrl };
   }
 
   const trackFilter = trackData.filter(({ previewUrl }) => previewUrl === songUrl)[0];
@@ -12,4 +12,4 @@ const playedSongsStruct = (trackData, songs, songUrl, setPlayedSongs) => {
 
 export default playedSongsStruct;
 
-// NEED TO REFACTOR PLAYEDSONGSSTRUCT
+// NEED TO REFACTOR PLAYEDSONGSSTRUCT AND DOCUMENTATION THIS FUNCTION
