5f18e585f3f441624953144825329d99b4f87ed1
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-05-03 16:11:59 -0300

    :fire: comment: redux-devtools in store folder

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import { applyMiddleware, createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(thunk)),
+  applyMiddleware(thunk),
 );
 
 export default store;
